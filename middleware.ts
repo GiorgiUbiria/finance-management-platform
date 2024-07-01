22f808a298929d2be14a07dece7cc4890b179ad2
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-07-01 20:55:42 +0400

    Add - Removed hardcoded path from SignIn and SignUp Clerk components. Updated middleware to correctly protect specified routes

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,15 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher([
+  '/',
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
+
+  return NextResponse.next();
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
