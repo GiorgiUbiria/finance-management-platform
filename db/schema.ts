5bdb6fee6f3d7a3a80da946b287caf1f5fd3ee77
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-07-02 19:52:12 +0400

    Add - Migration that modifies the accounts table plaidId field to be nullable. POST method on the /accounts endpoint that creates a new account entity

--- db/schema.ts
+++ db/schema.ts
@@ -4,7 +4,7 @@ import { createInsertSchema } from "drizzle-zod";
 
 export const accounts = pgTable("accounts", {
   id: text("id").primaryKey(),
-  plaidId: text("plaid_id").notNull(),
+  plaidId: text("plaid_id"),
   name: text("name").notNull(),
   userId: text("user_id").notNull(),
 })
---
+++ drizzle/0002_shocking_sasquatch.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "accounts" ALTER COLUMN "plaid_id" DROP NOT NULL;
