ef4dc49c2daf411a9aa816bbe4bd2c007baac3a3
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-07-02 20:39:34 +0400

    Add - Sonner toaster component to the layout. Create a new account mutation to create a new account entity. Updated sheet styles. Form ccomponent from shadcn-ui

--- features/accounts/components/new-account-sheet.tsx
+++ features/accounts/components/new-account-sheet.tsx
@@ -1,4 +1,8 @@
+import { z } from "zod";
+import { insertAccountSchema } from "@/db/schema";
 import { useNewAccount } from "../hooks/use-new-account";
+import { useCreateAccount } from "../api/use-create-account";
+import { AccountForm } from "./account-form";
 import {
   Sheet,
   SheetContent,
@@ -7,20 +11,37 @@ import {
   SheetTitle,
 } from "@/components/ui/sheet";
 
+const formSchema = insertAccountSchema.pick({ name: true });
+
+type FormValues = z.input<typeof formSchema>;
+
 export const NewAccountSheet = () => {
-  const {isOpen, onClose} = useNewAccount();
+  const { isOpen, onClose } = useNewAccount();
+
+  const mutation = useCreateAccount();
+
+  const onSubmit = (values: FormValues) => {
+    mutation.mutate(values, {
+      onSuccess: () => {
+        onClose();
+      },
+    });
+  };
 
   return (
     <Sheet open={isOpen} onOpenChange={onClose}>
       <SheetContent className="space-y-4">
         <SheetHeader>
-          <SheetTitle>
-              New Account
-          </SheetTitle>
+          <SheetTitle>New Account</SheetTitle>
           <SheetDescription>
             Create a new account to track your transactions.
           </SheetDescription>
         </SheetHeader>
+        <AccountForm
+          onSubmit={onSubmit}
+          disabled={mutation.isPending}
+          defaultValues={{ name: "" }}
+        />
       </SheetContent>
     </Sheet>
   );
