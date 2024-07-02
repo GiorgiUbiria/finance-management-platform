1d7d5fdf62619a600dcac90b502836f9006f382f
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-07-02 20:09:08 +0400

    Add - Working useNewAccount hook using zustand. New account sheet component

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { QueryProvider } from "@/providers/query-provider";
+import { SheetProvider } from "@/providers/sheet-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <html lang="en">
         <body className={inter.className}>
           <QueryProvider>
+            <SheetProvider />
             {children}
           </QueryProvider>
         </body>
