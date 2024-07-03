6ac4cc315ad448b9f16f754c027fbd20a496d41b
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-07-03 19:10:26 +0400

    Add - Implemented bulk delete accounts query hook

--- features/accounts/api/user-bulk-delete.ts
+++ features/accounts/api/user-bulk-delete.ts
@@ -1,26 +1,27 @@
 import { toast } from "sonner";
+
 import { InferRequestType, InferResponseType } from "hono";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import { client } from "@/lib/hono";
 
-type ResponseType = InferResponseType<typeof client.api.accounts.$post>;
-type RequestType = InferRequestType<typeof client.api.accounts.$post>["json"];
+type ResponseType = InferResponseType<typeof client.api.accounts["bulk-delete"]["$post"]>;
+type RequestType = InferRequestType<typeof client.api.accounts["bulk-delete"]["$post"]>["json"];
 
-export const useCreateAccount = () => {
+export const useBulkDeleteAccounts = () => {
   const queryClient = useQueryClient();
 
   const mutation = useMutation<ResponseType, Error, RequestType>({
     mutationFn: async (json) => {
-      const response = await client.api.accounts.$post({ json });
+      const response = await client.api.accounts["bulk-delete"]["$post"]({ json });
       return await response.json();
     },
     onSuccess: () => {
-      toast.success("Account created successfully");
+      toast.success("Accounts deleted successfully");
       queryClient.invalidateQueries({ queryKey: ["accounts"] });
     },
     onError: () => {
-      toast.error("Failed to create account");
+      toast.error("Failed to delete accounts");
     },
   });
 
