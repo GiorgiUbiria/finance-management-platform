f440f5b3c3c97788575ff52563e7240cb9ebe0cb
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-07-04 18:55:04 +0400

    Add - Sheet configuration for editing the account

---
+++ app/(dashboard)/accounts/actions.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+} from "@/components/ui/dropdown-menu";
+import { Edit, MoreHorizontal } from "lucide-react";
+
+import { useOpenAccount } from "@/features/accounts/hooks/use-open-account";
+
+type Props = {
+  id: string;
+};
+
+export const Actions = ({ id }: Props) => {
+  const {onOpen} = useOpenAccount();
+
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="size-8 p-0">
+            <MoreHorizontal className="size-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem
+            disabled={false}
+            onClick={() => onOpen(id)}
+          >
+            <Edit className="size-4 mr-2" />
+            Edit
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+};
--- app/(dashboard)/accounts/columns.tsx
+++ app/(dashboard)/accounts/columns.tsx
@@ -7,6 +7,7 @@ import { ArrowUpDown } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { ColumnDef } from "@tanstack/react-table";
 import { Checkbox } from "@/components/ui/checkbox";
+import { Actions } from "./actions";
 
 export type ResponseType = InferResponseType<
   typeof client.api.accounts.$get,
@@ -50,4 +51,8 @@ export const columns: ColumnDef<ResponseType>[] = [
       );
     },
   },
+  {
+    id: "actions",
+    cell: ({ row }) => <Actions id={row.original.id} />,
+  }
 ];
