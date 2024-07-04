dcd03b4feeb04ebf5a3c14b04338f74a03ef6c1a
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-07-04 19:02:36 +0400

    Add - Created useEditAccount hook that calls the corresponsding API method

---
+++ features/accounts/api/use-edit-account.ts
@@ -0,0 +1,36 @@
+import { toast } from "sonner";
+import { InferRequestType, InferResponseType } from "hono";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { client } from "@/lib/hono";
+
+type ResponseType = InferResponseType<
+  (typeof client.api.accounts)[":id"]["$patch"]
+>;
+type RequestType = InferRequestType<
+  (typeof client.api.accounts)[":id"]["$patch"]
+>["json"];
+
+export const useEditAccount = (id?: string) => {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async (json) => {
+      const response = await client.api.accounts[":id"]["$patch"]({
+        json,
+        param: { id },
+      });
+      return await response.json();
+    },
+    onSuccess: () => {
+      toast.success("Account updated successfully");
+      queryClient.invalidateQueries({ queryKey: ["account", { id }] });
+      queryClient.invalidateQueries({ queryKey: ["accounts"] });
+    },
+    onError: () => {
+      toast.error("Failed to edit account");
+    },
+  });
+
+  return mutation;
+};
