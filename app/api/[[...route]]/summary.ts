34f6c54e54db7935a80a098f8c376bcf6b97157d
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-07-24 13:49:58 +0400

    Add - Completed Summary API

--- app/api/[[...route]]/summary.ts
+++ app/api/[[...route]]/summary.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 import { z } from "zod";
 
-import { eq, and, inArray, gte, lte, desc, sql, sum } from "drizzle-orm";
+import { eq, and, inArray, gte, lte, desc, sql, sum, lt } from "drizzle-orm";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { zValidator } from "@hono/zod-validator";
 import { createId } from "@paralleldrive/cuid2";
@@ -14,6 +14,7 @@ import {
   categories,
   accounts,
 } from "@/db/schema";
+import { calculatePercentageChange, fillMissingDays } from "@/lib/utils";
 
 const app = new Hono()
   .get(
@@ -86,10 +87,90 @@ const app = new Hono()
         endDate
       )
 
+      const incomeChange = calculatePercentageChange(currentPeriod.income, lastPeriod.income);
+      const expensesChange = calculatePercentageChange(currentPeriod.expenses, lastPeriod.expenses);
+      const remainingChange = calculatePercentageChange(currentPeriod.remaining, lastPeriod.remaining);
+
+      const category = await db
+        .select(
+          {
+            name: categories.name,
+            value: sql`SUM(ABS(${transactions.amount}))`.mapWith(Number),
+          }
+        )
+        .from(transactions)
+        .innerJoin(
+          accounts,
+          eq(transactions.accountId, accounts.id)
+        )
+        .innerJoin(
+          categories,
+          eq(transactions.categoryId, categories.id)
+        )
+        .where(
+          and(
+            accountId ? eq(transactions.id, accounts.id) : undefined,
+            eq(accounts.userId, auth.userId),
+            lt(transactions.amount, 0),
+            gte(transactions.date, startDate),
+            lte(transactions.date, endDate),
+          )
+        )
+        .groupBy(
+          categories.name
+        )
+        .orderBy(
+          desc(
+            sql`SUM(ABS(${transactions.amount}))`
+          )
+        );
+
+      const topCategories = category.slice(0, 3);
+      const otherCategories = category.slice(3);
+      const otherSum = otherCategories.reduce((sum, current) => sum + current.value, 0)
+      const finalCategories = topCategories;
+      if (otherCategories.length > 0) {
+        finalCategories.push({
+          name: "Other",
+          value: otherSum,
+        })
+      }
+
+      const activeDays = await db
+        .select({
+          date: transactions.date,
+          income: sql`SUM(CASE WHEN ${transactions.amount} >= 0 THEN ${transactions.amount} ELSE 0 END)`.mapWith(Number),
+          expenses: sql`SUM(CASE WHEN ${transactions.amount} < 0 THEN ${transactions.amount} ELSE 0 END)`.mapWith(Number),
+        })
+        .from(transactions)
+        .innerJoin(
+          accounts,
+          eq(transactions.accountId, accounts.id)
+        )
+        .where(
+          and(
+            accountId ? eq(transactions.id, accounts.id) : undefined,
+            eq(accounts.userId, auth.userId),
+            gte(transactions.date, startDate),
+            lte(transactions.date, endDate),
+          )
+        )
+        .groupBy(transactions.date)
+        .orderBy(transactions.date);
+
+      const days = fillMissingDays(activeDays, startDate, endDate);
 
       return c.json({
-        currentPeriod,
-        lastPeriod,
+        data: {
+          remainingAmount: currentPeriod.remaining,
+          remainingChange,
+          incomeAmount: currentPeriod.income,
+          incomeChange,
+          expensesAmount: currentPeriod.expenses,
+          expensesChange,
+          categories: finalCategories,
+          days,
+        }
       });
     },
   );
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,5 @@
+import { eachDayOfInterval, isSameDay } from "date-fns";
+
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
@@ -19,4 +21,39 @@ export function formatCurrency(amount: number) {
     currency: "USD",
     minimumFractionDigits: 2,
   }).format(amount);
+}
+
+export function calculatePercentageChange(current: number, previous: number) {
+  if (previous === 0) {
+    return previous === current ? 0 : 100;
+  }
+
+  return ((current - previous) / previous) * 100;
+}
+
+export function fillMissingDays(activeDays: {date: Date, income: number, expenses: number}[], startDate: Date, endDate: Date) {
+  if (activeDays.length === 0) {
+    return [];
+  }
+
+  const allDays = eachDayOfInterval({
+    start: startDate,
+    end: endDate,
+  })
+
+  const transactionsByDay = allDays.map((day) => {
+    const found = activeDays.find((d) => isSameDay(d.date, day));
+
+    if (found) {
+      return found;
+    } else {
+      return {
+        date: day,
+        income: 0,
+        expnses: 0,
+      }
+    }
+  });
+
+  return transactionsByDay;
 }
