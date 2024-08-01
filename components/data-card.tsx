f94ef8a92b85a3a5c6a53761270581cbe3ade604
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-08-01 22:51:52 +0400

    Add - Data grid component. Data card component. Utility function to format percentage and date range for cards

---
+++ components/data-card.tsx
@@ -0,0 +1,114 @@
+import { VariantProps, cva } from "class-variance-authority";
+import { IconType } from "react-icons/lib";
+import { cn, formatCurrency, formatPercentage } from "@/lib/utils";
+
+import { Skeleton } from "@/components/ui/skeleton";
+import { CountUp } from "@/components/count-app";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle
+} from "@/components/ui/card";
+
+
+const boxVariant = cva(
+	"shrink-0 rounded-md p-3",
+	{
+		variants: {
+			variant: {
+				default: "bg-blue-500/20",
+				success: "bg-emerald-500/20",
+				danger: "bg-rose-500/20",
+				warning: "bg-yellow-500/20",
+			},
+		},
+		defaultVariants: {
+			variant: "default",
+		}
+	},
+);
+
+const iconVariant = cva(
+	"size-6",
+	{
+		variants: {
+			variant: {
+				default: "fill-blue-500",
+				success: "fill-emerald-500",
+				danger: "fill-rose-500",
+				warning: "fill-yellow-500",
+			},
+		},
+		defaultVariants: {
+			variant: "default",
+		}
+	},
+);
+
+type BoxVariants = VariantProps<typeof boxVariant>;
+type IconVariants = VariantProps<typeof iconVariant>;
+
+interface DataCardProps extends BoxVariants, IconVariants {
+	title: string;
+	value?: number;
+	percentageChange?: number;
+	dateRange: string;
+	icon: IconType;
+};
+
+export const DataCard = ({ title, variant, value = 0, percentageChange = 0, dateRange, icon: Icon }: DataCardProps) => {
+	return (
+		<Card className="border-none drop-shadow-sm">
+			<CardHeader className="flex flex-row items-center justify-between gap-x-4">
+				<div className="space-y-2">
+					<CardTitle className="text-xl line-clamp-1"> {title}</CardTitle>
+					<CardDescription className="line-clamp-1">
+						{dateRange}
+					</CardDescription>
+				</div>
+				<div className={cn(boxVariant({ variant }))}>
+					<Icon className={cn(iconVariant({ variant }))} />
+				</div>
+			</CardHeader>
+			<CardContent>
+				<h1 className="font-bold text-2xl break-all mb-2 line-clamp-1">
+					<CountUp
+						preserveValue
+						start={0}
+						end={value}
+						decimals={2}
+						decimalPlaces={2}
+						formattingFn={formatCurrency}
+					/>
+				</h1>
+				<p className={cn(
+					"text-muted-foreground text-sm line-clamp-1",
+					percentageChange > 0 && "text-emerald-500",
+					percentageChange < 0 && "text-rose-500",
+				)}>
+					{formatPercentage(percentageChange)} from last period
+				</p>
+			</CardContent>
+		</Card>
+	)
+}
+
+export const DataCardLoading = () => {
+	return (
+		<Card className="border-none drop-shadow-sm h-[192px]">
+			<CardHeader className="flex flex-row items-center justify-between gap-x-4">
+				<div className="space-y-2">
+					<Skeleton className="h-6 w-24" />
+					<Skeleton className="h-4 w-40" />
+				</div>
+				<Skeleton className="size-12" />
+			</CardHeader>
+			<CardContent>
+				<Skeleton className="shrink-0 h-10 w-24 mb-2" />
+				<Skeleton className="shrink-0 h-4 w-40" />
+			</CardContent>
+		</Card>
+	)
+}
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,4 @@
-import { eachDayOfInterval, isSameDay } from "date-fns";
+import { eachDayOfInterval, format, isSameDay, subDays } from "date-fns";
 
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
@@ -31,7 +31,7 @@ export function calculatePercentageChange(current: number, previous: number) {
   return ((current - previous) / previous) * 100;
 }
 
-export function fillMissingDays(activeDays: {date: Date, income: number, expenses: number}[], startDate: Date, endDate: Date) {
+export function fillMissingDays(activeDays: { date: Date, income: number, expenses: number }[], startDate: Date, endDate: Date) {
   if (activeDays.length === 0) {
     return [];
   }
@@ -56,4 +56,41 @@ export function fillMissingDays(activeDays: {date: Date, income: number, expense
   });
 
   return transactionsByDay;
+}
+
+type Period = {
+  from: string | Date | undefined;
+  to: string | Date | undefined;
+};
+
+export function formartDateRange(period?: Period) {
+  const defaultTo = new Date();
+  const defaultFrom = subDays(defaultTo, 30);
+
+  if (!period?.from) {
+    return `${format(defaultFrom, "LLL dd")} - ${format(defaultTo, "LLL dd, y")}`;
+  }
+
+  if (period.to) {
+    return `${format(period.from, "LLL dd")} - ${format(period.to, "LLL dd, y")}`;
+  }
+
+  return `${format(period.from, "LLL dd")}`;
+};
+
+export function formatPercentage(
+  value: number,
+  options: { addPrefix?: boolean } = {
+    addPrefix: false,
+  },
+) {
+  const result = new Intl.NumberFormat("en-US", {
+    style: "percent",
+  }).format(value / 100);
+
+  if (options.addPrefix && value > 0) {
+    return `+${result}`;
+  }
+
+  return result;
 }
